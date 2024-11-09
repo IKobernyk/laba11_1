619218e56c88c3c07b29bbbbf6006e4f8dd3ac5a
Admin <Admin@DESKTOP-C20V9DS>
2024-11-09 12:04:49 +0200

    Добавьте файлы проекта.

---
+++ laba11t1/laba11t1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    const int N = 12;  // кількість елементів
+    int a[N];
+
+    // Початкові значення
+    a[0] = 1;
+    a[1] = 2;
+
+    // Обчислення елементів послідовності
+    for (int i = 2; i < N; ++i) {
+        a[i] = a[i - 1] + a[i - 2];
+    }
+
+    // Виведення результату
+    for (int i = 0; i < N; ++i) {
+        cout << "a[" << i << "] = " << a[i] << endl;
+    }
+
+    return 0;
+}
